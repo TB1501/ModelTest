40d0bbf825607d991ed53742d41c3a70899cf41d
Tin Bašić <tin.basic22@gmail.com>
2025-01-21 17:07:00 +0100

    Updated model

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,9 +1,12 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/ml.hpp>
+#include <iostream>
+#include "roundNumber.h"
 
 int main() {
 
 	//Load the trained model
-	cv::Ptr<cv::ml::ANN_MLP> mlp = cv::ml::ANN_MLP::load("C:\\Users\\Tin\\Downloads\\trainedModel_6\\trainedModel_6.xml");
+	cv::Ptr<cv::ml::ANN_MLP> mlp = cv::ml::ANN_MLP::load("C:\\Users\\Tin\\Desktop\\newtrainData\\trData3.xml");
 	if (mlp.empty()) {
 		std::cerr << "Error: Failed to load the model." << std::endl;
 		return -1;
@@ -18,7 +21,7 @@ int main() {
 	}
 
 	//Load the image you want to classify
-	cv::Mat testImage = cv::imread("C:\\Users\\Tin\\Desktop\\Vsite\\3 godina\\Zavrsni\\ModelTest\\TestSamples\\img_1.jpg", cv::IMREAD_GRAYSCALE);
+	cv::Mat testImage = cv::imread("C:\\Users\\Tin\\Desktop\\Vsite\\3 godina\\Zavrsni\\ModelTest\\TestSamples\\img_9.jpg", cv::IMREAD_GRAYSCALE);
 
 	//resize the image to the image size that we made the model from
 	cv::resize(testImage, testImage, cv::Size(28, 28));
@@ -28,8 +31,22 @@ int main() {
 	//Flatten the image
 	cv::Mat flattenedImage = testImage.reshape(1, 1);
 	cv::Mat input;
-	flattenedImage.convertTo(input, CV_32F, 1.0/255.0);
-	std::cout << input.size;
+	cv::Mat input_Normalized;
+	flattenedImage.convertTo(input, CV_32F);
+
+
+	for (int row = 0; row < input.rows; ++row)
+	{
+		for (int col = 0; col < input.cols; ++col)
+		{
+			float value = input.at<float>(row, col);
+			// Rounding the value to 4 decimal places
+			input.at<float>(row, col) = roundNumber(value, 10000);
+		}
+	}
+
+	std::cout << input_Normalized.size;
+
 	
 
 	//Perform prediction
