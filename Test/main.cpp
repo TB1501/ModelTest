39a3fa12f623856b064be209950bacad52a3024c
Tin Bašić <tin.basic22@gmail.com>
2025-01-27 14:36:53 +0100

    Update. Edited relative paths

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,12 +1,12 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/ml.hpp>
 #include <iostream>
-#include "roundNumber.h"
+
 
 int main() {
 
 	//Load the trained model
-	cv::Ptr<cv::ml::ANN_MLP> mlp = cv::ml::ANN_MLP::load("C:\\Users\\Tin\\Desktop\\newtrainData\\trData3.xml");
+	cv::Ptr<cv::ml::ANN_MLP> mlp = cv::ml::ANN_MLP::load("./TrainedModel/trData1.xml");
 	if (mlp.empty()) {
 		std::cerr << "Error: Failed to load the model." << std::endl;
 		return -1;
@@ -21,33 +21,17 @@ int main() {
 	}
 
 	//Load the image you want to classify
-	cv::Mat testImage = cv::imread("C:\\Users\\Tin\\Desktop\\Vsite\\3 godina\\Zavrsni\\ModelTest\\TestSamples\\img_9.jpg", cv::IMREAD_GRAYSCALE);
+	cv::Mat testImage = cv::imread("./TestSamples/img_0.jpg", cv::IMREAD_GRAYSCALE);
 
 	//resize the image to the image size that we made the model from
 	cv::resize(testImage, testImage, cv::Size(28, 28));
 
 
-
 	//Flatten the image
 	cv::Mat flattenedImage = testImage.reshape(1, 1);
 	cv::Mat input;
-	cv::Mat input_Normalized;
 	flattenedImage.convertTo(input, CV_32F);
 
-
-	for (int row = 0; row < input.rows; ++row)
-	{
-		for (int col = 0; col < input.cols; ++col)
-		{
-			float value = input.at<float>(row, col);
-			// Rounding the value to 4 decimal places
-			input.at<float>(row, col) = roundNumber(value, 10000);
-		}
-	}
-
-	std::cout << input_Normalized.size;
-
-	
 
 	//Perform prediction
 	cv::Mat output;
@@ -62,7 +46,7 @@ int main() {
 	int predictedClass = classIdPoint.x;
 
 	//Display the result
-	std::cout << "Predicted class: " << predictedClass << " With confidence: " << confidence << std::endl;
+	std::cout << "Predicted number: " << predictedClass << " With confidence: " << confidence << std::endl;
 
 	return 0;
 
